f840ba25d9c2fc16dc2e5e8262d18901766b1171
Nota <60175653+Nota30@users.noreply.github.com>
2021-07-26 00:31:49 +0600

    forgot to enable redis again

--- src/app.ts
+++ src/app.ts
@@ -3,22 +3,20 @@ import cookieParser from 'cookie-parser';
 import createError from 'http-errors';
 import logger from 'morgan';
 import path from 'path';
-//import redis from 'redis';
+import redis from 'redis';
 
 //Routes
 import indexRouter from './routes/index';
 import userRouter from './routes/user';
 import characterRouter from './routes/character';
-import profileRouter from './routes/profile';
 
 const app = express();
-/*
 const client = redis.createClient(6379);
 
 client.on('error', (err) => {
     console.log(err);
 });
-*/
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.set('json spaces', 2);
@@ -28,16 +26,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-/*
+
 app.use(function (req: Request, res, next) {
     res.locals = client;
     next();
 });
-*/
+
 app.use('/', indexRouter);
 app.use('/user', userRouter);
 app.use('/character', characterRouter);
-app.use('/profile', profileRouter);
 
 app.use(function (req, res, next) {
     next(createError(404));
